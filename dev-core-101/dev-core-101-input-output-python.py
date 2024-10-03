6d0456f9479eefe4bfcba96fafce8ad28312dce3
Andrey8705 <Andrey.shadrin.1998@mail.ru>
2024-10-03 12:34:47 +0000

    Create input-output python file

---
+++ dev-core-101/dev-core-101-input-output-python.py
@@ -0,0 +1,9 @@
+import datetime
+
+Name = input("Как тебя зовут? ")
+Age = int(input("Год твоего рождения в формате(YYYY)? "))
+Today = datetime.date.today()
+Formateddate = int(Today.strftime("%Y"))
+a = 100
+Years100 =((Age + a) - Formateddate)
+print(Name, ", через ", Years100," года тебе будет 100 лет")
