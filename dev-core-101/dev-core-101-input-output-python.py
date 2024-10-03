1c9cb6049d316c05dd68e37b07b298074e59ab40
Andrey8705 <Andrey.shadrin.1998@mail.ru>
2024-10-03 12:38:59 +0000

    edited version

--- dev-core-101/dev-core-101-input-output-python.py
+++ dev-core-101/dev-core-101-input-output-python.py
@@ -6,4 +6,5 @@ Today = datetime.date.today()
 Formateddate = int(Today.strftime("%Y"))
 a = 100
 Years100 =((Age + a) - Formateddate)
-print(Name, ", через ", Years100," года тебе будет 100 лет")
+Year = Formateddate + Years100
+print(Name, ", через ", Years100," года тебе будет 100 лет.Это будет ", Year," год.")
