7464dc5f9caf2f9d799691c6cae43586e1565802
Andrey8705 <Andrey.shadrin.1998@mail.ru>
2024-10-03 10:51:09 +0000

    Create task python

---
+++ dev-core-101/dev-core-101-task-python.py
@@ -0,0 +1,100 @@
+import time
+
+#Чистим зубы(Уровень HARD)
+a = print("Просыпаемся")
+b = print("Идём в ванную")
+c = print("Открываем дверь")
+d = print("Открываем воду")
+e = print("Открываем тюбик зубной пасты")
+f = print("Берём щетку")
+g = print("Наносим зубную пасту на щётку")
+h = print("Открываем рот")
+i = print("Начинаем чистить зубы")
+g = print("Чистим первый зуб")
+k = print("Чистим второй зуб")
+l = print("Чистим третий зуб")
+n = print("Чистим четвертый зуб")
+m = print("Чистим пятый зуб")
+o = print("Чистим шестой зуб")
+p = print("Чистим седьмой зуб")
+q = print("Чистим восьмой зуб")
+r = print("Чистим девятый зуб")
+s = print("Передохнули")
+t = print("Чистим десятый зуб")
+u = print("Чистим двенадцатый зуб, потому что у нас нет одиннадцатого зуба, но было весёлое детство")
+v = print("Чистим тринадцатый зуб")
+w = print("Чистим четырнадцатый зуб")
+x = print("Чистим пятнадцатый зуб")
+y = print("Чистим шестнадцатый зуб")
+z = print("Вспоминаем что сегодня у нас выходной")
+a1 = print("Чуть-чуть злимся")
+b1 = print("Полоскаем рот")
+c1 = print("Закрываем кран")
+d1 = print("Выходим из ванной")
+e1 = print("Ложимся в кровать")
+f1 = print("Засыпаем")
+a
+time.sleep(1)
+b
+time.sleep(1)
+c
+time.sleep(1)
+d
+time.sleep(1)
+e
+time.sleep(1)
+f
+time.sleep(1)
+g
+time.sleep(1)
+h
+time.sleep(1)
+i
+time.sleep(1)
+g
+time.sleep(1)
+k
+time.sleep(1)
+l
+time.sleep(1)
+n
+time.sleep(1)
+m
+time.sleep(1)
+o
+time.sleep(1)
+p
+time.sleep(1)
+q
+time.sleep(1)
+r
+time.sleep(1)
+s
+time.sleep(1)
+t
+time.sleep(1)
+u
+time.sleep(1)
+v
+time.sleep(1)
+w
+time.sleep(1)
+x
+time.sleep(1)
+y
+time.sleep(1)
+z
+time.sleep(1)
+a1
+time.sleep(1)
+b1
+time.sleep(1)
+c1
+time.sleep(1)
+d1
+time.sleep(1)
+e1
+time.sleep(1)
+f1
+time.sleep(1)
+
