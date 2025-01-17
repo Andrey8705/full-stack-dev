2cda168ea9c85fb4823cb91a68d65070f9025e36
Andrey8705 <Andrey.shadrin.1998@mail.ru>
2025-01-17 21:39:21 +0500

    Create frontend structure for MVP. Create astro components, create main page.

---
+++ MVP-project/front-end/gullible-group/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        padding: '20px',
+        textAlign: 'center',
+        background: '#0d0d0d',
+        color: '#fff',
+        borderTop: '1px solid #333',
+      }}
+    >
+      <p>© 2025 OneOfTwins. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
