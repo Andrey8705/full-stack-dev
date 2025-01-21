e5d1ed45127c4ce5465501bf2d7825ededda1c9a
Andrey8705 <Andrey.shadrin.1998@mail.ru>
2025-01-21 05:59:51 +0500

    POST request to the backend is configured.

--- MVP-project/backend/app/main.py
+++ MVP-project/backend/app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.security import OAuth2PasswordBearer
 from routers import capsuleRouters, userRouters
 from database.db_setup import create_db
+from fastapi.middleware.cors import CORSMiddleware
 
 
 load_dotenv()
@@ -15,5 +16,13 @@ def startup():
 
 app.include_router(userRouters.router, prefix="/api", tags=["Users"])
 app.include_router(capsuleRouters.router, prefix="/api", tags=["Capsule"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Можно ограничить определенные источники, например, ['http://localhost:3000']
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешить все методы (GET, POST, и т.д.)
+    allow_headers=["*"],  # Разрешить все заголовки
+)
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
