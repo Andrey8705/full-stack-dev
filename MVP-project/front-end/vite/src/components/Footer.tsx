dfe5a831ec5a1bcf82dd337d2477d4f368984c55
Andrey8705 <Andrey.shadrin.1998@mail.ru>
2025-01-30 00:21:10 +0500

    Fix styles in MVP.

--- MVP-project/front-end/vite/src/components/Footer.tsx
+++ MVP-project/front-end/vite/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-900 text-white px-1 mt-[99vh] border-t-2  border-cyan-400 rounded-xl text-center">
+    <footer className="bg-gray-900 text-white px-1 mb-[1vh] border-t-2 mt-4  justify-self-center border-cyan-400 rounded-xl text-center">
       <div>
         <p className="my-2">
           © {new Date().getFullYear()} OneOfTwins Time Capsule. All rights reserved.
