0ce0761ab5866dbac5d3ac87b13f68924f2dc9c7
Andrey8705 <Andrey.shadrin.1998@mail.ru>
2025-01-30 08:23:05 +0500

    Fix capsuleRouters.py change HTTTPExeption on JSONResponse

--- MVP-project/backend/app/routers/capsuleRouters.py
+++ MVP-project/backend/app/routers/capsuleRouters.py
@@ -1,3 +1,4 @@
+from fastapi.responses import JSONResponse
 from models.capsuleBaseModel import CapsuleCreate
 from database.db_setup import get_db
 from sqlalchemy.orm import Session
@@ -20,13 +21,13 @@ def create_capsule(capsule: CapsuleCreate, db: Session = Depends(get_db), token:
     try:
         open_date = datetime.strptime(capsule.unlock_date, "%Y-%m-%d")
     except ValueError:
-        raise HTTPException(
+        return JSONResponse(
             status_code=400,
             detail="Invalid date format or invalid date. Use ('2025-01-20')."
         )
 
     if open_date <= create_date_now:
-        raise HTTPException(status_code=400,detail="Open date can't be < created date")
+        return JSONResponse(status_code=400,detail="Open date can't be < created date")
 
     new_capsule = Capsule(
         name = capsule.name,
@@ -39,7 +40,7 @@ def create_capsule(capsule: CapsuleCreate, db: Session = Depends(get_db), token:
     db.add(new_capsule)
     db.commit()
     db.refresh(new_capsule)    
-    return {"message": f"Capsule {new_capsule.name} successfuly created. {time_to_open} days before opening the capsule"}
+    return JSONResponse({"message": f"Capsule {new_capsule.name} successfuly created. {time_to_open} days before opening the capsule"})
 
 @router.get("/admin/get/capsule/all")
 def get_capsules(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
@@ -47,18 +48,18 @@ def get_capsules(token: str = Depends(oauth2_scheme), db: Session = Depends(get_
     check_user_role(user_data, "admin")
     capsules = db.query(Capsule).all()
 
-    return capsules
+    return JSONResponse(capsules)
 
 @router.get("/admin/get/capsule/{id}")
 def get_capsule_by_id(id : int, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     user_data =  verify_acces_token(token)
     capsule = db.query(Capsule).filter(Capsule.id == id).first()
     if not capsule:
-        raise HTTPException(status_code=404, detail="Capsule not found")
+        return JSONResponse(status_code=404, detail="Capsule not found")
     
     check_user_and_capsule_user_id(user_data["id"], capsule.user_id)
 
-    return capsule
+    return JSONResponse(capsule)
 
 @router.delete("/admin/delete/capsule/{id}")
 def delete_capsule(id: int, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
@@ -66,11 +67,11 @@ def delete_capsule(id: int, token: str = Depends(oauth2_scheme), db: Session = D
     check_user_role(user_data, "admin")
     capsule = db.query(Capsule).filter(Capsule.id == id).first()
     if not capsule:
-        raise HTTPException(status_code=404, detail="Capsule not found")
+        return JSONResponse(status_code=404, detail="Capsule not found")
     db.delete(capsule)
     db.commit()
 
-    return{"message":f"{capsule["name"]} successfuly deleted"}
+    return JSONResponse({"message":f"{capsule["name"]} successfuly deleted"})
 
 @router.put("/admin/edit/capsule/{id}")
 def edit_capsule(id: int, capsule_data: CapsuleCreate, token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
@@ -78,10 +79,10 @@ def edit_capsule(id: int, capsule_data: CapsuleCreate, token: str = Depends(oaut
     check_user_role(user_data, "admin")
     capsule = db.query(Capsule).filter(Capsule.id == id).first()
     if not capsule:
-        raise HTTPException(status_code=404, detail="Capsule not found")
+        return JSONResponse(status_code=404, detail="Capsule not found")
     
     capsule.name = capsule_data.name
     capsule.unlock_date = capsule_data.unlock_date
     capsule.message = capsule_data.message
     db.commit()
-    return capsule
+    return JSONResponse(capsule)
