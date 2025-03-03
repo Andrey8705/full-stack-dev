44a5a8143d6a40fce443cfda0c9f84baac7aa7f5
Andrey8705 <Andrey.shadrin.1998@mail.ru>
2025-03-04 00:13:27 +0500

    Update interface My-Capsules

--- MVP-project/front-end/vite/src/components/MyCapsules.tsx
+++ MVP-project/front-end/vite/src/components/MyCapsules.tsx
@@ -4,7 +4,8 @@ import { getMyCapsules } from "../app/service/Service.ts";
 interface Capsule {
   id: number;
   name: string;
-  create_date: string; // Исправил created_at → create_date
+  create_date: string;
+  unlock_date: string;
 }
 
 const MyCapsules = () => {
@@ -16,29 +17,33 @@ const MyCapsules = () => {
     });
   }, []);
 
-  const formatDate = (dateString: string | undefined) => {
-    if (!dateString) return "Неизвестная дата";
+const formatDate = (dateString: string | undefined) => {
+  if (!dateString) return "Неизвестная дата";
 
-    const parsedDate = Date.parse(dateString);
-    if (isNaN(parsedDate)) return "Неверный формат даты";
+const parsedDate = Date.parse(dateString);
+  if (isNaN(parsedDate)) return "Неверный формат даты";
 
-    return new Date(parsedDate).toLocaleDateString();
-  };
+  return new Date(parsedDate).toLocaleDateString();
+};
 
   return (
-    <div>
-      <h1>Мои капсулы</h1>
-      {capsules.length > 0 ? (
-        <ul>
-          {capsules.map((capsule) => (
-            <li key={capsule.id}>
-              {capsule.name} - {formatDate(capsule.create_date)}
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Нет капсул</p>
-      )}
+    <div className="flex flex-col items-center mt-10">
+      <h1 className="text-3xl font-bold text-[#f5deb3]">Мои капсулы</h1>
+        <div className="flex flex-col w-[57vh] bg-center bg-size bg-no-repeat items-center gap-4 mt-5 rounded-full p-6 from-gray-900 to-black opacity-90 text-[#ffe4c4]">
+          {capsules.length > 0 ? (
+            <ul className="text-left">
+              {capsules.map((capsule) => (
+                <li key={capsule.id} className="w-[400px] bg-white/10 p-6 rounded-xl shadow-lg text-white">
+                  <h2 className="text-xl font-semibold mb-2">Title: {capsule.name}</h2>
+                  <p className="text-gray-300">Created at: {new Date(capsule.create_date).toLocaleDateString()}</p>
+                  <p className="text-gray-300">Unlock date: {formatDate(capsule.unlock_date)}</p>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>Нет капсул</p>
+          )}
+      </div>
     </div>
   );
 };
